0f932f23cc58cc680c45981c94becb8e1b55ef2b
irakliShengelia <iraklishengelia94@gmail.com>
2021-04-30 08:12:28 +0000

    Added Dynamic title support

--- functions.php
+++ functions.php
@@ -1,4 +1,14 @@
 <?php
+
+
+function reviewstore_theme_support(){
+    //Adds Support for title tag in wp_head()
+    add_theme_support( 'title-tag' );
+}
+
+add_action('after_setup_theme','reviewstore_theme_support');
+
+
 function reviewstore_register_styles(){
 
     $version = wp_get_theme()->get( 'Version' );
